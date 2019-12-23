0e25ece12258a2d2e54e927d3601599ae03fd9fd
CircleCI docs-deploy job <jrediger@mozilla.com>
2019-12-23 09:43:28 +0000

    [skip ci] Updates

---
+++ docs/implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["glean_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"glean_core/enum.Lifetime.html\" title=\"enum glean_core::Lifetime\">Lifetime</a>",synthetic:false,types:["glean_core::common_metric_data::Lifetime"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
