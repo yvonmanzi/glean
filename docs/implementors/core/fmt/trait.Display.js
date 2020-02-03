3cb8ee179193072e54a7722edbb5e33a8ec9182c
CircleCI docs-deploy job <jrediger@mozilla.com>
2020-02-03 19:04:07 +0000

    [skip ci] Updates

--- docs/implementors/core/fmt/trait.Display.js
+++ docs/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["glean_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"glean_core/struct.Error.html\" title=\"struct glean_core::Error\">Error</a>",synthetic:false,types:["glean_core::error::Error"]},];
+implementors["glean_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"glean_core/struct.Error.html\" title=\"struct glean_core::Error\">Error</a>","synthetic":false,"types":["glean_core::error::Error"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/implementors/std/error/trait.Error.js
+++ docs/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["glean_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glean_core/struct.Error.html\" title=\"struct glean_core::Error\">Error</a>",synthetic:false,types:["glean_core::error::Error"]},];
+implementors["glean_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glean_core/struct.Error.html\" title=\"struct glean_core::Error\">Error</a>","synthetic":false,"types":["glean_core::error::Error"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
